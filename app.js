b467206ac410c356b12fc1bd2fc41efc0c5a7c36
elyassin1997 <92631854+elyassin1997@users.noreply.github.com>
2023-08-08 11:46:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const qrcode = require('qrcode');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth} = require('whatsapp-web.js');
 
 const app = express();
 const PORT = 3000;
@@ -12,9 +12,10 @@ app.get('/login',  (req, res) => {
     let canAdd =true;
     const clientId = req.query.key;
     const client = new Client({
-        authStrategy: new LocalAuth({ clientId })
+        authStrategy: new LocalAuth({ clientId }),
     });
     client.on('qr', (qr) => {
+        console.log(`QrCode Ready For : ${clientId}`);
         if(canAdd){
             qrcode.toDataURL(qr, (err, url) => {
                 canAdd=false;
@@ -24,14 +25,17 @@ app.get('/login',  (req, res) => {
                   res.send(`Scan QR code for Client ${clientId}`+`<br><img src='${url}'>`);
                 }
               });
+        }else{
+            client.destroy();
+            console.log('Client Destroyed!'+`${clientId}`);
         }
         
     });
     client.on('authenticated', () => {
-        console.log('Authentication Succefully');
+        console.log('Authentication Succefully : '+`${clientId}`);
     });
     client.on('auth_failure', () => {
-        console.log('Authentication Error');
+        console.log('Authentication Error : '+`${clientId}`);
     });
     client.on('ready', () => {
         const xclient = client.info.wid._serialized;
@@ -49,9 +53,21 @@ app.get('/login',  (req, res) => {
             console.log("Admin Loged In : "+client.info.wid._serialized);
         }
     });
+   client.on("disconnected",(reason)=>{
+        client.logout();
+        console.log('Client Disconnected : '+`${clientId}`);
+    })
     
-    
+    app.get('/message',  (req, res) => {
+        const to = req.query.to;
+        const id = req.query.id;
+        const msg = req.query.msg;
+        const fromInfo = clients.find((x)=>x.id==id);
+        const from = fromInfo.user;
+        from.sendMessage(to+"@c.us",msg);
+        res.send('OK!');
 
+    })
     
 
     client.initialize();
@@ -61,4 +77,3 @@ app.get('/login',  (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
